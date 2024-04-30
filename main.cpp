9c7ecf78727b84b55c4b8668c1e98cf2d34fe41f
AnTINooB001 <tvershinin375@gmail.com>
2024-04-30 22:54:37 +0300

    test

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <unistd.h>
+#include <map>
 
 #define MENU_HEIGHT 2
 
@@ -32,6 +34,10 @@
 #define CTRL_Y 0x19
 #define CTRL_Z 0x1A
 #define KEY_ESC 0x1B
+#define FILE_SEP_KEY 0x1C
+#define GROUP_SEP_KEY 0x1D
+#define REC_SEP_KEY 0x1E
+#define UNIT_SEP_KEY 0x1F
 #define HOR_TAB CTRL_I
 #define LINE_FEED CTRL_J
 
@@ -40,18 +46,75 @@ void moveLeft();
 void moveRight();
 void moveDown();
 void moveUp();
+void goToRowEnd();
+void goToRowStart();
+void goToLastRow();
+void goToFirstRow();
 
 class editor
 {
 private:
+    std::vector<std::string> words;
     std::vector<std::string> rows;
     std::string message;
     std::string displaying_name;
     std::string file_name;
+private:
+     std::vector<std::string> delim(std::string str, std::string del)
+     {
+         std::vector<std::string> res;
+         for (auto it = del.begin(); it < del.end(); it++) {
+             std::string buff;
+             size_t pos;
+             while((pos = str.find(*it)) != std::string::npos) {
+                 buff = str.substr(0,pos);
+                 res.push_back(buff);
+                 str.erase(0,pos+del.length());
+             }
+         }
+         auto it = res.begin();
+         auto e = res.end()-1;
+         bool ch = false;
+         while(it <= e) {
+             if(it->empty()) {
+                 res.erase(it);
+                 it = res.begin();
+                 e = res.end()-1;
+                 continue;
+             }
+             if(e->empty()) {
+                 res.erase(e);
+                 it = res.begin();
+                 e = res.end()-1;
+                 continue;
+             }
+             it++;
+             e--;
+         }
+         return res;
+     }
+
+     void getWords()
+     {
+         std::ifstream file("bin/Econf.txt");
+         if(!file.is_open())
+             return;
+         std::string to_parse;
+         while(1)
+         {
+             if(file.eof())
+                 break;
+             std::string buff;
+             std::getline(file,buff);
+             to_parse.append(buff + " ");
+         }
+         words = delim(to_parse," ");
+     }
+
 public:
     int xoff,yoff;
     bool modif;
-    editor(): xoff{0},yoff{0} { rows.push_back(""); modif = false; };
+    editor(): xoff{0},yoff{0} { rows.push_back(""); modif = false; getWords(); };
 
     void setFileName(std::string str) //
     {
@@ -94,6 +157,7 @@ public:
         rows.insert(it,str);
     }
 
+
     void printMenu()
     {
         int txtsz = 0;
@@ -110,16 +174,23 @@ public:
                 printw(" Saved |");
                 txtsz += 8;
             }
+            addch(' ');
             int sz = width-txtsz-2;
             if(sz < displaying_name.size()-1) {
+                std::string dots = "...";
+                sz -= dots.length();
                 for (size_t i = 0; i < sz; i++)
                 {
                     addch(displaying_name[i]);
                 }
+                printw(dots.c_str());
                 addch('|');
             }
             else
                 printw(" %s |",displaying_name.c_str());
+            move(height-MENU_HEIGHT+1,0);
+            printw("| CTRL+S to save |");
+            printw(" CTRL+D to search |");
         }
         else{
             printw("%s",message.c_str());
@@ -143,11 +214,36 @@ public:
         int ty = 0;
         clear();
         move(0,0);
-        for(int y = 0; y < height-MENU_HEIGHT; y++) {
-            if(y+yoff < rows.size()) {
-                int sz = rows[y+yoff].size();
+        for(int y = yoff; y < height-MENU_HEIGHT; y++) {
+            if(y < rows.size()) {
+                int sz = rows[y].size();
                 if(sz-xoff > 0) {
-                    insnstr(&rows[y+yoff].c_str()[xoff],rows[y+yoff].size()-xoff);
+                    std::vector<int> poses;
+                    for(auto& el : words)
+                        poses.push_back(searchWordInRow(el));
+                    std::string str = "str";
+                    int pos = searchWordInRow(str);
+                    int size;
+                    if(sz > width+xoff)
+                        size = width+xoff;
+                    else
+                        size = sz;
+                    for (int i = xoff; i < size; i++)
+                    {
+                        if(i >= pos && i < pos+str.length() && pos != -1) {
+                            attrset(COLOR_PAIR(1));
+                            addch(rows[y].c_str()[i] | COLOR_PAIR(1));
+                        }
+                        else {
+                            attrset(COLOR_PAIR(0));
+                            addch(rows[y].c_str()[i] | COLOR_PAIR(0));
+                        }
+                        if(i >= pos+str.length())
+                            pos = searchWordInRow(str,pos+1);
+                        
+                    }
+                    
+                    //insnstr(&rows[y+yoff].c_str()[xoff],rows[y+yoff].size()-xoff);
                 }
             }
             else
@@ -211,13 +307,20 @@ public:
             addMessage("ECS to cancel | Enter name of the file: ");
             printRows();
             while(c = getch()) {
-                if(c == KEY_ESC)
-                    break;
+                if(c == KEY_ESC){
+                    eraseMessage();
+                    printRows();
+                    return;
+                }
                 if(c == '\n' && !buff.empty())
                     break;
                 if(c == '\n')
                     continue;
-                else if(isdigit(c) || isalpha(c) || c == '.')
+                if( c == KEY_BACKSPACE || c == KEY_DL) {
+                    if(buff.end()-1 >= buff.begin())
+                        buff.erase(buff.end()-1);
+                }
+                else if(isalnum(c) || c == '.')
                     buff.push_back(c);
                 addMessage(buff);
                 printRows();
@@ -243,6 +346,38 @@ public:
     
         file.close();
         modif = false;
+    }
+
+    int searchWordInText(std::string str)
+    {
+        goToFirstRow();
+        goToRowStart();
+        
+        
+        int pos,y;
+        while (1)
+        {
+            y = getcury(stdscr) + yoff;
+            if((pos = rows[y].find(str)) != std::string::npos)
+                return pos;
+            else if(y == rowsCount())
+                break;
+            else
+                moveDown();
+        }
+        goToRowEnd();
+        return -1;
+    }
+
+    int searchWordInRow(std::string str,int i = 0)
+    {
+        int pos,y;
+        y = getcury(stdscr) + yoff;
+        if(y == rowsCount())
+           return -1;
+        if((pos = rows[y].find(str,i)) != std::string::npos)
+            return pos;
+        return -1;
     }
 };
 editor E;
@@ -325,6 +460,7 @@ void moveLeft()
 
 void moveRight()
 {
+    
     int pos_x = getcurx(stdscr);
     int pos_y = getcury(stdscr);
 
@@ -426,11 +562,18 @@ void addChar(char c)
         E.insertChar((char)c);              
         moveRight();
     }
+
+
 }
 
 void endProg(const int a)
 {
+    noraw();
     echo();
+    clear();
+    keypad(stdscr,FALSE);
+    refresh();
+    endwin();
     exit(a);
 }
 
@@ -460,6 +603,7 @@ void keyProcess()
     int pos_x = getcurx(stdscr);
     int pos_y = getcury(stdscr);
     int count;
+    std::string search;
     switch (c) {
         case CTRL_Q:
             if(E.modif == false) {
@@ -485,10 +629,43 @@ void keyProcess()
             E.writeInFile();
             break;
 
+        case CTRL_D:
+            while (1)
+            {
+                E.eraseMessage();
+                E.addMessage("ESC to cancel | ");
+                E.addMessage(search);
+                E.printRows();
+
+                c = getKey();
+                if(c == KEY_ESC) {
+                    E.eraseMessage();
+                    E.printRows();
+                    return;
+                }
+                else if(c == '\n') {
+                    count = E.searchWordInText(search);
+                    break;
+                }
+                else if( c == KEY_BACKSPACE || c == KEY_DL) {
+                    if(search.end()-1 >= search.begin())
+                        search.erase(search.end()-1);
+                }
+                else if(isalnum(c)){
+                    search.push_back(c);
+                }
+            }
+            if(count > 0)
+                while (count--)
+                    moveRight();
+            
+            E.eraseMessage();
+            E.printRows();
+            break;
+
         case CTRL_A: // not realesed
         case CTRL_B:
         case CTRL_C:
-        case CTRL_D:
         case CTRL_E:
         case CTRL_F:
         case CTRL_G:
@@ -507,6 +684,10 @@ void keyProcess()
         case CTRL_X:
         case CTRL_Y:
         case CTRL_Z:
+        case FILE_SEP_KEY:
+        case GROUP_SEP_KEY:
+        case REC_SEP_KEY:
+        case UNIT_SEP_KEY:
             break;
 
         case HOR_TAB:
@@ -533,7 +714,7 @@ void keyProcess()
             break;
 
         case KEY_NPAGE:
-            count = height-MENU_HEIGHT;      
+            count = height-MENU_HEIGHT-1;      
             while(pos_y != count) {         //move to down edge of screen
                 moveDown();
                 pos_y = getcury(stdscr);
@@ -600,13 +781,14 @@ void keyProcess()
 
 int main(int argc, char* argv[]) {
     initscr();
-
+    start_color();
+    init_pair(1,COLOR_BLUE,COLOR_BLACK);
     getmaxyx(stdscr,height,width);
 
     if(argv[1] != nullptr)
         openFile(argv[1]);
     else
-        openFile("");
+        openFile("/home/antinoob/CLionProjects/ncurTextEdit/bin/test.txt");
     
 
     halfdelay(0);
@@ -616,9 +798,10 @@ int main(int argc, char* argv[]) {
     keypad(stdscr,TRUE);
 
     while (1) {
+        int i = E.xoff;
+        int w = width+i;
         E.printRows();  
         keyProcess();
     }
-    clear();
-    endwin();
+    endProg(0);
 }
